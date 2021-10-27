7ba9ca7e16c5d93385bfd4eaa3883739cd9cb845
brad <brad@brad.sh>
2021-10-27 19:14:59 +0100

    Added code to demonstrate the use of a while loop with a counter.

---
+++ basics/repetitions/while_loop/count.py
@@ -0,0 +1,11 @@
+# Ask how many cables Beep needs to avoid
+print("How many lives cables must I avoid?")
+avoid_cables = int(input())
+
+cables_avoided = 0
+
+# Avoid the cables
+while cables_avoided < avoid_cables:
+    print("Avoiding...", end="")
+    cables_avoided = cables_avoided + 1
+    print(f"Done! {cables_avoided} cables avoided.")
